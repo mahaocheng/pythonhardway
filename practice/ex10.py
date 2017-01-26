3393abe64a53a9f549cda41f03d43453a11ca35e
mahaocheng <mahaocheng1985@126.com>
2017-01-26 09:34:43 +0800

    ex10~13

---
+++ practice/ex10.py
@@ -0,0 +1,28 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip \n\t* Grass
+"""
+ma_hao = "\\\'\"\a\b\f\n\Nr\t\u12\U11\v"
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+print ma_hao
+while True:
+    for i in ["/", "-", "|", "\\", "|"]:
+       print "%r\r" % i,
+ 
+
+hao = '''
+dafafadf
+dafa
+dafadfa
+dafafa
+'''
+print hao
--- practice/ex8.py
+++ practice/ex8.py
@@ -5,22 +5,15 @@ print formatter % ("one", "two", "three", "four")
 print formatter % (True, False, False, True)
 print formatter % (formatter, formatter, formatter, formatter)
 print formatter % (
-    "I had this thing.",
-    "That you could type up right.",
-    "But it didn't sing.",
-    "So I said goodnight."
-)
+	"I had this thing.",
+	"That you could type up right.",
+	"But it didn't sing.",
+	"So I said goodnight."
+	)
 formatter = "%s %s %s %s"
 
-print formatter % (1, 2, 3, 4)
-print formatter % ("one", "two", "three", "four")
-print formatter % (True, False, False, True)
-print formatter % (formatter, formatter, formatter, formatter)
-print formatter % (
-    "I had this thing.",
-        "That you could type up right.",
-	    "But it didn't sing.",
-	        "So I said goodnight."
-		)
-
-		
+1 2 3 4
+'one' 'two' 'three' 'four'
+True False False True
+'%r %r %r %r' '%r %r %r %r' '%r %r %r %r' '%r %r %r %r'
+'I had this thing.' 'That you could type up right.' "But it didn't sing." 'So I said goodnight.'
